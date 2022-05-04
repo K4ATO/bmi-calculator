98942a4e4e4a93a5fc2cde0e4aa3e660b5ae01ae
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-04 04:38:07 +0200

    finish app Structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBmi] = useState("");
+  const [message, setMessage] = useState("");
+  let imgSrc = "";
+  return (
+    <div className="app">
+      <div className="container">
+        <h2 className="center">BMI Calculator</h2>
+        <form>
+          <div>
+            <label>Weight</label>
+            <input value={weight} />
+          </div>
+          <div>
+            <label>Height</label>
+            <input value={height} />
+          </div>
+          <div>
+            <button className="btn" type="submit">
+              Submit
+            </button>
+            <button className="btn btn-outline" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+
+        <div className="center">
+          <h3>Your BMI is: {bmi}</h3>
+          <p>{message}</p>
+        </div>
+
+        <div className="img-container">
+          <img src={imgSrc} alt=""></img>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
